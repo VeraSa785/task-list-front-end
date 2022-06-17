df43f30e3feed9de05c714500e4328f63e09d565
verasazonova <vera.zhokhova@gmail.com>
2022-06-17 13:43:02 -0700

    Done with update toggle complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+import { useState } from 'react';
 
 const App = () => {
+  const TASKS = [
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ];
+
+  const [tasks, setTasks] = useState(TASKS);
+
+  const toggleComplete = (id) => {
+    console.log('toggled', id);
+    const updatedTasks = [...tasks];
+
+    for (let task of updatedTasks) {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+    }
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} toggleComplete={toggleComplete} />}</div>
       </main>
     </div>
   );
